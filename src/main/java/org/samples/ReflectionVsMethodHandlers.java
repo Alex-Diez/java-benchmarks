e577e5177919e93c5cfe536034164320faea67c6
Alex-Diez <alex.dukhno@icloud.com>
2018-11-11 11:45:07 +0200

    improvements to refletction vs method handle benchmark

--- src/main/java/org/samples/ReflectionVsMethodHandlers.java
+++ src/main/java/org/samples/ReflectionVsMethodHandlers.java
@@ -1,12 +1,5 @@
 package org.samples;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Fork;
@@ -18,164 +11,97 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
 @State(Scope.Benchmark)
+@Fork(value = 3)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
 @BenchmarkMode(Mode.AverageTime)
-@Fork(1)
-@Warmup(iterations = 5)
-@Measurement(iterations = 5, timeUnit = TimeUnit.NANOSECONDS)
+@Warmup(iterations = 5, time = 1)
+@Measurement(iterations = 5, time = 1)
 public class ReflectionVsMethodHandlers {
 
-    private static final String ARRAY_FOLDING_METHOD_NAME = "arrayFoldingExperimentCalculation";
-    private static final String RETURN_CONSTANT_METHOD_NAME = "returnConstant";
-    private static final String FIELD_INCREMENT_METHOD_NAME = "fieldIncrement";
+  private static final MethodHandles.Lookup lookup = MethodHandles.lookup();
 
-    private static final MethodHandle staticFinalArrayFoldingMethodHandling;
-    private static final MethodHandle staticFinalReturnConstantMethodHandling;
-    private static final MethodHandle staticFinalFieldIncrementMethodHandling;
+  private int[] data = new int[16];
 
-    private int[] data = new int[1024];
-    private int field;
+  private Method arrayFoldingReflect;
+  private MethodHandle arrayFoldingMH;
 
-    static {
-        MethodHandles.Lookup lookup = MethodHandles.lookup();
-        MethodType type = MethodType.methodType(int.class);
-        try {
-            staticFinalArrayFoldingMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, ARRAY_FOLDING_METHOD_NAME, type);
-            staticFinalReturnConstantMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, RETURN_CONSTANT_METHOD_NAME, type);
-            staticFinalFieldIncrementMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, FIELD_INCREMENT_METHOD_NAME, type);
-        } catch (NoSuchMethodException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
+  private Method substringReflect;
+  private MethodHandle substringMH;
+  private MethodHandle boundSubstringMH;
+
+  @Setup
+  public void initData() throws Exception {
+    Random random = new Random();
+    for (int i = 0; i < data.length; i++) {
+      data[i] = random.nextInt();
     }
+    arrayFoldingReflect = getClass().getMethod("arrayFolding");
+    substringReflect = String.class.getMethod("substring", int.class, int.class);
 
-    private Method arrayFoldingReflection;
-    private MethodHandle arrayFoldingMethodHandling;
-    private final MethodHandle finalArrayFoldingMethodHandling;
-    private Method returnConstantReflection;
-    private MethodHandle returnConstantMethodHandling;
-    private final MethodHandle finalReturnConstantMethodHandling;
-    private Method fieldIncrementReflection;
-    private MethodHandle fieldIncrementMethodHandling;
-    private final MethodHandle finalFieldIncrementMethodHandling;
+    MethodType intReturnType = MethodType.methodType(int.class);
+    arrayFoldingMH = lookup.findVirtual(ReflectionVsMethodHandlers.class, "arrayFolding", intReturnType);
 
-    public ReflectionVsMethodHandlers() {
-        MethodHandles.Lookup lookup = MethodHandles.lookup();
-        MethodType type = MethodType.methodType(int.class);
-        try {
-            finalArrayFoldingMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, ARRAY_FOLDING_METHOD_NAME, type);
-            finalReturnConstantMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, RETURN_CONSTANT_METHOD_NAME, type);
-            finalFieldIncrementMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, FIELD_INCREMENT_METHOD_NAME, type);
-        } catch (NoSuchMethodException | IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
 
-    @Setup
-    public void initData() throws Exception {
-        Random random = new Random();
-        for (int i = 0; i < data.length; i++) {
-            data[i] = random.nextInt();
-        }
-        arrayFoldingReflection = getClass().getMethod(ARRAY_FOLDING_METHOD_NAME);
-        returnConstantReflection = getClass().getMethod(RETURN_CONSTANT_METHOD_NAME);
-        fieldIncrementReflection = getClass().getMethod(FIELD_INCREMENT_METHOD_NAME);
-        MethodHandles.Lookup lookup = MethodHandles.lookup();
-        MethodType type = MethodType.methodType(int.class);
-        arrayFoldingMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, ARRAY_FOLDING_METHOD_NAME, type);
-        returnConstantMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, RETURN_CONSTANT_METHOD_NAME, type);
-        fieldIncrementMethodHandling = lookup.findVirtual(ReflectionVsMethodHandlers.class, FIELD_INCREMENT_METHOD_NAME, type);
-    }
-    
-    public int returnConstant() {
-        return 42;
-    }
+    MethodType substringType = MethodType.methodType(String.class, int.class, int.class);
+    substringMH = lookup.findVirtual(String.class, "substring", substringType);
+    boundSubstringMH = substringMH.bindTo("Hello, World!");
+  }
 
-    public int fieldIncrement() {
-        return field++;
+  public int arrayFolding() {
+    int[] data = Arrays.copyOf(this.data, this.data.length);
+    int sum = 0;
+    for (int i : data) {
+      sum += i;
     }
+    return sum;
+  }
 
-    public int arrayFoldingExperimentCalculation() {
-        int[] data = Arrays.copyOf(this.data, this.data.length);
-        int sum = 0;
-        for (int i : data) {
-            sum += i;
-        }
-        return sum;
-    }
+  @Benchmark
+  public int arrayFoldingBaseline() {
+    return arrayFolding();
+  }
 
-    @Benchmark
-    public int arrayFoldingBaseline() {
-        return arrayFoldingExperimentCalculation();
-    }
+  @Benchmark
+  public int arrayFoldingReflectCall() throws Exception {
+    return (int) arrayFoldingReflect.invoke(this);
+  }
 
-    @Benchmark
-    public int arrayFoldingReflectionCall() throws Exception {
-        return (int) arrayFoldingReflection.invoke(this);
-    }
+  @Benchmark
+  public int arrayFoldingMHCall() throws Throwable {
+    return (int) arrayFoldingMH.invoke(this);
+  }
 
-    @Benchmark
-    public int arrayFoldingMethodHandlerCall() throws Throwable {
-        return (int) arrayFoldingMethodHandling.invokeExact(this);
-    }
+  @Benchmark
+  public String stringConstBaseline() {
+    return "Hello";
+  }
 
-    @Benchmark
-    public int finalArrayFoldingMethodHandlerCall() throws Throwable {
-        return (int) finalArrayFoldingMethodHandling.invokeExact(this);
-    }
+  @Benchmark
+  public String substringBaseline() {
+    return "Hello, World!".substring(0, 5);
+  }
 
-    @Benchmark
-    public int staticFinalArrayFoldingMethodHandlerCall() throws Throwable {
-        return (int) staticFinalArrayFoldingMethodHandling.invokeExact(this);
-    }
+  @Benchmark
+  public String substringReflectCall() throws Throwable {
+    return (String) substringReflect.invoke("Hello, World!", 0, 5);
+  }
 
-    @Benchmark
-    public int returnConstantBaseline() {
-        return returnConstant();
-    }
+  @Benchmark
+  public String substringMHCall() throws Throwable {
+    return (String) substringMH.invoke("Hello, World!", 0, 5);
+  }
 
-    @Benchmark
-    public int returnConstantReflectionCall() throws Exception {
-        return (int) returnConstantReflection.invoke(this);
-    }
-
-    @Benchmark
-    public int returnConstantMethodHandlerCall() throws Throwable {
-        return (int) returnConstantMethodHandling.invokeExact(this);
-    }
-
-    @Benchmark
-    public int finalReturnConstantMethodHandlerCall() throws Throwable {
-        return (int) finalReturnConstantMethodHandling.invokeExact(this);
-    }
-
-    @Benchmark
-    public int staticFinalReturnConstantMethodHandlerCall() throws Throwable {
-        return (int) staticFinalReturnConstantMethodHandling.invokeExact(this);
-    }
-
-    @Benchmark
-    public int fieldIncrementBaseline() {
-        return fieldIncrement();
-    }
-
-    @Benchmark
-    public int fieldIncrementReflectionCall() throws Exception {
-        return (int) fieldIncrementReflection.invoke(this);
-    }
-
-    @Benchmark
-    public int fieldIncrementMethodHandlerCall() throws Throwable {
-        return (int) fieldIncrementMethodHandling.invokeExact(this);
-    }
-
-    @Benchmark
-    public int finalFieldIncrementMethodHandlerCall() throws Throwable {
-        return (int) finalFieldIncrementMethodHandling.invokeExact(this);
-    }
-
-    @Benchmark
-    public int staticFinalFieldIncrementMethodHandlerCall() throws Throwable {
-        return (int) staticFinalFieldIncrementMethodHandling.invokeExact(this);
-    }
+  @Benchmark
+  public String boundSubstringMHCall() throws Throwable {
+    return (String) boundSubstringMH.invoke(0, 5);
+  }
 }
